bb73fe26260c8f39362f412d8b03f3737f457748
James Gartrell <jgartrel@gmail.com>
2023-12-22 21:24:44 -0800

    Initial commit

---
+++ Makefile
@@ -0,0 +1,32 @@
+#
+# Setup all targets to repeatedly run as tasks
+#
+.PHONEY: all install install-local test run clean distclean
+all: help
+
+install: ## Install bundler dependencies
+	bundle install --path vendor/bundle --binstubs --clean
+	bundle package
+
+install-local: ## Install bundler dependencies from local cache
+	bundle install --path vendor/bundle --binstubs --clean --local
+
+run:    ## Run a development server
+	bundle exec ./parse_tsd.rb
+
+testargs ?= --order random
+ifneq ($(strip $(seed)),)
+  testargs += --seed $(seed)
+endif
+
+test:   ## Run all tests
+	bundle exec rspec $(testargs) ./spec
+
+clean:  ## Remove all generated files
+	rm -rf vendor/bundle bin tmp
+
+distclean:  ## Remove all non-versioned files
+	git clean -f -x -d
+
+help:
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-20s\033[0m %s\n", $$1, $$2}'
---
+++ parse_tsd.rb
@@ -0,0 +1,80 @@
+#!/usr/bin/env ruby
+
+require 'bindata'
+require 'pp'
+
+class TSD_Record < BinData::Record
+  endian :little
+
+#  struct :fft_rank do 
+#    uint16 index
+#    uint16 magnitude
+#  end
+ 
+  struct :tsd_data do 
+    uint64 :timestamp
+    uint32 :millisTime
+    uint8  :state
+    uint8  :hwFlags
+    uint8  :swFlags
+    uint8  :alertLevel
+    uint16 :alertCount
+    uint16 :motionCount
+    uint16 :micCount
+    uint16 :vDDH
+    uint16 :vBat
+    uint16 :vSys
+    uint16 :vIn
+    uint16 :iChg
+    int16  :tempCore
+    int16  :tempAlt
+    int16  :x
+    int16  :y
+    int16  :z
+    int16  :tareX
+    int16  :tareY
+    int16  :tareZ
+    uint8  :heartRate
+    uint8  :breathRate
+    uint8  :chargeStat
+    uint8  :zoneCount
+    float_le    :zAngle
+    float_le    :zPitch
+    float_le    :zRoll
+
+    uint64 :pauseUntilTime
+    uint32 :resetReason
+    uint16 :heapFree
+    uint16 :paddingOne
+    uint32 :accelSampleCount
+    uint32 :accelSamplePersisted
+    uint32 :sensorTimeMillis
+    uint16 :numSamples
+    uint8  :odrSize
+    uint8  :fifoFlags
+
+    uint16 :breathRateErrors
+    uint16 :breathRateNonPeak
+
+    uint16 :dcOffset
+    uint16 :dcOffsetBinCount
+
+#    S_FFT_RANK_T fftRank[10]
+    array :fftRank, initial_length: 10 do
+      uint16 :bin
+      uint16 :magnitude
+    end
+  end
+
+  def pretty_print(pp)
+    tsd_data.each_pair do |k, v|
+      pp.text "#{k}:".ljust(20)
+      pp.text "#{v}\n"
+    end
+  end
+end
+
+puts "Hello World"
+
+file      = File.open("test3.bin")
+pp header = TSD_Record.read(file)
