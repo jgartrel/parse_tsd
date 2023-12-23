127a7bcb1b0f3e12f30648a4bfb2d3d42f812874
James Gartrell <jgartrel@gmail.com>
2023-12-22 23:46:55 -0800

    Add header parsing for TSD file

--- parse_tsd.rb
+++ parse_tsd.rb
@@ -3,6 +3,55 @@
 require 'bindata'
 require 'pp'
 
+class Unique_ID < BinData::Record
+  endian :little
+  uint32 :val1
+  uint32 :val2
+  uint32 :val3
+  uint32 :val4
+
+  def to_s
+    "0x%.8x%.8x%.8x%.8x" % [val4, val3, val2, val1]
+  end
+
+  def pretty_print(pp)
+    pp.text self.to_s
+  end
+
+  def snapshot
+    self.to_s
+  end
+
+end
+
+class TSD_Header < BinData::Record
+  endian :little
+
+  uint64    :timestamp
+  int8      :timezoneOffset
+  string    :gitVersion, length: 63, trim_padding: true
+  unique_id :boardID
+  uint32    :serialNum
+  int16     :tareX
+  int16     :tareY
+  int16     :tareZ
+  uint8     :alertMode
+  uint8     :emptyPaddingOne
+  uint32    :uptimeMillis
+end
+
+class TSD_Data < BinData::Record
+  endian :little
+
+  string     :fileID, length: 4
+  uint32     :fileVersion
+  uint32     :headerSize
+  uint32     :emptyPadding
+  tsd_header :header
+  string     :dataID, length: 4
+  uint32     :recordSize
+end
+
 class TSD_Record_V2 < BinData::Record
   endian :little
 
@@ -70,5 +119,8 @@ end
 
 puts "Hello World"
 
-file      = File.open("./test_data/test3.bin")
-pp header = TSD_Record_V2.read(file)
+#file      = File.open("./test_data/test3.bin")
+#pp header = TSD_Record_V2.read(file)
+file      = File.open("./test_data/23121000.TSD")
+pp tsd_data = TSD_Data.read(file)
+pp tsd_data.header.boardID
